db6597f285ae15164c97e3e21d8829cd0b0fceab
Beaudinn <beaudinngreve@gmail.com>
2023-08-24 11:46:07 +0200

    Update ServiceProvider.php

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -12,13 +12,15 @@ class ServiceProvider extends AddonServiceProvider
     {
         parent::boot();
 
-        Utility::make('static-cache-manager')
-            ->title(__('Static Cache Manager'))
-            ->description(__('Clear specific paths in your static cache.'))
-            ->view('static-cache-manager::index')
-            ->routes(function ($router) {
-                $router->post('/clear', ClearController::class)->name('clear');
-            })
-            ->register();
+		Utility::extend(function () {
+			Utility::register('static-cache-manager')
+				->title(__('Static Cache Manager'))
+				->description(__('Clear specific paths in your static cache.'))
+				->view('static-cache-manager::index')
+				->icon('hammer-wrench')
+				->routes(function ($router) {
+					$router->post('/clear', ClearController::class)->name('clear');
+				});
+		});
     }
 }
